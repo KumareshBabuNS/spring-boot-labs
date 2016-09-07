4ebe0d6be1e8061af6f5ad05cc148e09c5b2f6dd
dwong-pivotal <derrick.chwong@gmail.com>
2016-09-08 00:31:40 +0800

    zipkin working

--- zipkin-service/src/main/java/com/example/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/com/example/ZipkinServiceApplication.java
@@ -3,12 +3,20 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinServiceApplication {
+
+	@Bean
+	public Sampler defaultSampler() {
+		return new AlwaysSampler();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinServiceApplication.class, args);
