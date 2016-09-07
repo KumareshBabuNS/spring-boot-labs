fd8f2817614df528c8a2fb89cbe8bda9cbd28a91
dwong-pivotal <derrick.chwong@gmail.com>
2016-09-08 00:58:53 +0800

    use sleuth zipkin

--- address-book-service/src/main/java/com/example/AddressBookServiceApplication.java
+++ address-book-service/src/main/java/com/example/AddressBookServiceApplication.java
@@ -14,6 +14,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AddressBookServiceApplication {
--- zipkin-service/src/main/java/com/example/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/com/example/ZipkinServiceApplication.java
@@ -3,20 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.Sampler;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-import org.springframework.context.annotation.Bean;
+import zipkin.server.EnableZipkinServer;
 
 @EnableDiscoveryClient
-@EnableZipkinStreamServer
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServiceApplication {
-
-	@Bean
-	public Sampler defaultSampler() {
-		return new AlwaysSampler();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinServiceApplication.class, args);
